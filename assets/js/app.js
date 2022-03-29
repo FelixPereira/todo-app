e3334bc8b19187283444c76ab28ad2e01985c3bf
felixpereira538@gmail.com <felixpereira538@gmail.com>
2022-03-29 08:16:40 +0100

    Display tasks on HTML page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,33 +12,101 @@ const modal = document.querySelector(".container_modal");
 const closeModalBtn = document.querySelector(".modal_btn-no");
 const backdrop = document.querySelector(".backdrop");
 const clearCompconstedBtn = document.querySelector(".btn_clear-compconsted");
-const allItems = [];
+const showActiveTask = document.querySelector('.show-active');
+const showCompletedTask = document.querySelector('.show-completed');
+const showAllTask = document.querySelector('.show-all');
 
+const allTasks = [
+  {id: 1, task: 'Study hooks', status: 'active'},
+  {id: 2, task: 'Play footbal', status: 'active'},
+  {id: 3, task: 'Go to shop', status: 'completed'},
+  {id: 4, task: 'Eat', status: 'completed'}
+];
 
-function addNewTask() {
-  const userTask = addItemInput.value;
-  const newTask = {
-    itemContent: userTask,
-    itemId: Math.trunc(Math.random() * 200)
-  };
+// NUMBER OF REMAINING TASKS
+itemsLeft.textContent = allTasks.length;
 
-  addItemInput.value = ' ';
-  allItems.push(newTask);
-  allItems.forEach(task => {
-    const newTaskHTML = `
+function displayTasks(tasks) {
+  tasks.forEach(task => {
+    const html =`
       <li class="todo_list-item">
         <div class="todo-item">
           <img class="icon-check" src="assets/images/icon-check.svg" alt="Icon check">
-          <span class="item-content">${task.itemContent}</span>
-        </div>
-        <img class="remove-item" src="assets/images/icon-cross.svg" alt="Icone cross">
-      </li>
+          <span class="item-content">${task.task}</span>
+          </div>
+            <img class="remove-item" src="assets/images/icon-cross.svg" alt="Icone cross">
+      </li
     `;
-    todoList.insertAdjacentHTML('afterbegin', newTaskHTML)
-
+    
+    // ADD ITEM TO HTML DOCUMENT
+    todoList.insertAdjacentHTML('afterbegin', html);
   })
+};
 
-  console.log(allItems);
-}
+function updateUI(tasks) {
+  // CLEAR EXISTING ITEMS
+  todoList.innerHTML = '';
 
-addItemBtn.addEventListener('click', addNewTask)
+  addItemInput.value = '';
+  if(tasks.length > 0) {
+    displayTasks(tasks);
+    entryText.hidden = true;
+  } else {
+      entryText.hidden = false;
+    }
+};
+
+updateUI(allTasks);
+
+// ADD NEW ITEM TO ARRAY
+function addNewTask() {
+  const task = addItemInput.value;
+  if(task === '') {
+    alert('Invalid task');
+  } else {
+      const newTask = {id: Math.trunc((Math.random() * 100)), task, status: 'active'};
+  
+      allTasks.push(newTask);
+  
+      updateUI(allTasks);
+  }
+};
+
+function showActive() {
+  const tasks = allTasks
+    .slice(0)
+    .filter(task => task.status === 'active');
+    
+  showActiveTask.classList.add('active');
+  showCompletedTask.classList.remove('active');
+  showAllTask.classList.remove('active');
+
+  updateUI(tasks);
+};
+
+function showCompleted() {
+  const tasks = allTasks
+    .slice(0)
+    .filter(task => task.status === 'completed');
+
+  showActiveTask.classList.remove('active');
+  showCompletedTask.classList.add('active');
+  showAllTask.classList.remove('active');
+
+  updateUI(tasks);
+};
+
+function showAll() {
+  showActiveTask.classList.remove('active');
+  showCompletedTask.classList.remove('active');
+  showAllTask.classList.add('active');
+
+  updateUI(allTasks);
+};
+
+
+//  EVENT LISoTENERS
+addItemBtn.addEventListener('click', addNewTask);
+showActiveTask.addEventListener('click', showActive);
+showCompletedTask.addEventListener('click', showCompleted);
+showAllTask.addEventListener('click', showAll);
